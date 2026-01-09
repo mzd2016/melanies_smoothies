2e7cfdcd5fb7fb88be801c9b765adbff95f2d046
mzd2016 <mzd2016@med.cornell.edu>
2026-01-09 11:56:58 -0500

    Update streamlit_app.py
    Fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefruit_response)
+st.text(smoothiefroot_response)
